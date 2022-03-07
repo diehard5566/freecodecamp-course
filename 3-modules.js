109f660f0df34801fbcfc390ae032fbbc2a3e539
diehard5566 <shihyao001@gmail.com>
2022-03-07 15:43:07 +0800

    first commit

---
+++ 3-modules.js
@@ -0,0 +1,9 @@
+const names = require('./4-name');
+const sayHi = require('./5-uitls');
+const data = require('./6-alt-flavor');
+require('./7-mind-grenade');
+
+sayHi('susan')
+sayHi(names.john);
+sayHi(names.peter);
+ 
